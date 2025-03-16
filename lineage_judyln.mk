00adfabd0ec3f9ffaf025b96850d40d6b8cb0e6c
Han Sol Jin <hansol@hansol.ca>
2025-03-15 21:30:53 -0700

    judyln: remove PRODUCT_SOONG_NAMESPACES
    There is nothing to namespace.
    
    Change-Id: I8fa70bc6b26f0dbb50cb0149ae39c91f9195a449

--- lineage_judyln.mk
+++ lineage_judyln.mk
@@ -17,9 +17,6 @@
 # common judyln
 $(call inherit-product, device/lge/judyln-common/judyln-common.mk)
 
-PRODUCT_SOONG_NAMESPACES += \
-	$(DEVICE_PATH)
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/lge/judyln/judyln-vendor.mk)
 
